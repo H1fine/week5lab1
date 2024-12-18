06aae3bf0046f4820c4af9fa7422e05ca7eaa42e
Jasmine Boyd <boyd.jasmine45@gmail.com>
2024-12-17 21:14:06 -0600

    Practice Code

---
+++ script.js
@@ -0,0 +1,52 @@
+"use strict";
+
+let typeanimal1 = "Dog" //Corgie
+let typeanimal2 = "Cat" //Siamese
+let typeanimal3 = "Bird" //Macaw
+
+let answer1 = prompt("Corgie");
+let answer2 = prompt("Siamese");
+let answer3 = prompt("Mccaw");
+
+let lifespan1 = "15 years";
+let lifespan2 = "20 years";
+let lifespan3 = "30 years";
+
+
+if (typeanimal1 === question1) {
+    document.write (correct);
+}
+
+    else  (typeanimal1 === answer2){
+        document.write (incorrect);
+    }
+
+    else if (typeanimal1 === answer3){
+        document.write (incorect);
+    }
+
+if (typeanimal2 === answer2){
+    document.write (correct);
+
+    else if (typeanimal2 === answer3){
+        document.write(incorrect);
+    }
+
+    else if (typeanimal2 === answer1){
+        document.write(incorrect);
+    }
+}
+
+if (typeanimal3 === answer3){
+    document.write (correct);
+
+    else if (typeanimal3 === answer2){
+        document.write(incorrect);
+    }
+
+    else if (typeanimal3 === answer1){
+        document.write(incorrect);
+    }
+}
+
+}
